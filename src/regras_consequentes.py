a1cc7c25bfd1430fd5b85fd172bc29966380b0a3
jAzz-hub <joaogu.2001@gmail.com>
2025-05-25 16:33:43 -0300

    <feat>:: Consertando conjuntos Fuzzy e começando adequação

---
+++ src/regras_consequentes.py
@@ -0,0 +1,44 @@
+
+
+
+f_Sobe_abruptamente =  {'p': 3, 'q':0}, #f
+h_Desce_abruptamente = {'p': -1.3, 'q':1.93} #h
+
+    # Comportamentos Menos Abruptos (transições moderadas)
+i_Desce_Menos_Abruptamente = {'p': -1.76, 'q': 2.27} #i
+j_Sobe_menos_abruptamente = {'p': 0, 'q': 1.83} #j  #Cobre 1.5-2  
+k_Desce_Menos_Abruptamente_2 = {'p':1.804, 'q': -3.345} #k #Cobre 2-2.5
+
+    # Comportamentos Suaves
+l_Desce_muito_suavemente = {'p':-1.73, 'q':5.5} #l # Cobre 2.5-3
+
+m_Desce_Abruptamente = {'p': -1.78, 'q': 5.57} #m  # Cobre 3-3.5
+
+n1_Desce_muito_muito_suavemente_2 = {'p': , 'q':}#n1
+n2_Sobe_suavemente_2 = {'p': , 'q': }#n2
+
+
+o_Sobe = {'p': , 'q': }
+p1_plato= {'p': , 'q': }
+p2_desce_muito_suavemente_2 = {'p': , 'q': }
+
+
+    # Comportamentos_suaves Adicionais
+Sobe_abruptamente = {'p':, 'q':}         # Cobre 6-6.5
+Sobe_suavemente = {'p':, 'q':}           # Cobre 6-6.5
+desce_muito_muito_suavemente 2 = {'p':, 'q':}# Cobre 6.96-7.5
+desce_muito_muito_suavemente = {'p':, 'q':}bre 7.5-7.82 e 7.82-8
+Sobe_muito_suavemente 2 = {'p':, 'q':} # Cobre 8-8.5
+Sobe_muito_muito_suavemente 2 = {'p':, 'q':}re 8.5-8.63 e 8.63-9
+desce_suavemente = {'p':, 'q':}        # Cobre 9-10
+
+
+    "Desce muito muito suavemente": [3.48, 3.52, 3.7, 3.75], #n1       # Cobre 3.5-3.74
+    "Sobe suavemente": [3.745, 3.8, 4.1, 4.2], #n2      # Cobre 3.74-4 
+
+    # Comportamentos Suaves
+    "Sobe": [4.15, 4.3, 4.4, 4.5], #o       # Cobre 4-4.5 
+
+    "platô" : [4.45, 4.5, 4.6, 4.68] #p1 Cobre 4.5- 4.68
+    "Desce muito muito suavemente" :  [4.65, 4.7, 4.9, 5] #p2 Cobre 4.65 - 5
+
